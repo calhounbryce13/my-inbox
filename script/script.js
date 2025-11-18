38fe09a7939a1feed0e61cfff3d02c03395c61ee
calhounbryce13 <111155940+calhounbryce13@users.noreply.github.com>
2025-11-18 10:07:08 -0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -6,11 +6,14 @@ document.addEventListener('DOMContentLoaded', async() => {
         const response = await fetch('https://calhounbryce13-backend.onrender.com/messages', {method: "GET"});
         if(response.status == 200){
             let data = await response.json();
+            console.log(data);
             data = Array.from(data);
+            console.log(data);
+
             data.forEach((mssg) => {
                 const message = document.createElement('div');
                 message.classList.add('message');
-                message.textContent = mssg.message;
+                message.textContent = mssg;
                 document.getElementById('data').appendChild(message);
             })
         }
